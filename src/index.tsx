36c6b49b7b95d5672f65c762d734cb9305e5d780
hnhaidev <hnhai.dev@gmail.com>
2022-03-11 10:50:48 +0700

    update styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Styles } from './styles';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Styles />
     <BrowserRouter>
       <App />
     </BrowserRouter>
